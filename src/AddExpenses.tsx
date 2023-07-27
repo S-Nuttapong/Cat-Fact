3958b573351dfb31f49761a120d1dcc2cc914afd
Nuttapong <Nuttapong@nuttapongs-MBP.ST-244F>
2023-07-27 16:56:23 +0700

    feat: 'add expenses' feature

---
+++ src/AddExpenses.tsx
@@ -0,0 +1,71 @@
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Spinner,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { expenseServices, fetchCatFact } from "./Apis";
+import { ExpenseForm } from "./ExpenseForm";
+
+export const AddExpense = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const queryClient = useQueryClient();
+
+  const catFact = useQuery("catFact", fetchCatFact, { enabled: isOpen });
+
+  const addExpenseMutation = useMutation(expenseServices.addExpense, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("expenses");
+      onClose();
+    },
+  });
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add Expense</Button>
+      {isOpen && (
+        <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add Expenses</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={5}>
+              <SimpleGrid
+                columns={{ sm: 1, md: 2 }}
+                columnGap={10}
+                minChildWidth={300}
+              >
+                <ExpenseForm
+                  onSubmit={async (data) =>
+                    await addExpenseMutation.mutate(data)
+                  }
+                />
+                <Flex
+                  flexDir="column"
+                  justifyContent="start"
+                  alignItems="start"
+                >
+                  <Text m={0}>Random cat fact:</Text>
+                  {catFact.isFetching ? (
+                    <Spinner />
+                  ) : (
+                    <Text mt={2}>{catFact.data}</Text>
+                  )}
+                </Flex>
+              </SimpleGrid>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      )}
+    </div>
+  );
+};
---
+++ src/ExpenseForm.tsx
@@ -0,0 +1,106 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Input,
+  Select,
+  Stack,
+  chakra,
+} from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ReactNode } from "react";
+import { FieldError, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Expense } from "./Apis";
+
+const expenseSchema = z.object({
+  item: z
+    .string()
+    .nonempty({ message: "Item is required" })
+    .regex(/^[a-zA-Z0-9 ]*$/, "Item cannot include special characters."),
+  category: z.enum(["Food", "Furniture", "Accessory"]),
+  amount: z
+    .string()
+    .nonempty({ message: "Amount is required" })
+    .refine((value) => !isNaN(Number(value)), {
+      message: "Amount must be a valid number",
+    })
+    .refine((value) => Number(value) > 0, {
+      message: "Amount must be a positive number",
+    }),
+});
+
+interface IExpenseForm {
+  onSubmit: (data: Omit<Expense, "id">) => Promise<void>;
+}
+
+interface IFormContainer {
+  error?: FieldError;
+  children: ReactNode;
+  label: string;
+}
+
+const FormField = ({ error, label, children }: IFormContainer) => (
+  <FormControl isInvalid={!!error}>
+    <Box w="full">
+      <HStack alignItems="baseline">
+        <FormLabel w={90}>{`${label}:`}</FormLabel>
+        <Flex w="full">{children}</Flex>
+      </HStack>
+      <HStack>
+        <Box w={105} />
+        <Flex w="full">
+          <FormErrorMessage>{error?.message}</FormErrorMessage>
+        </Flex>
+      </HStack>
+    </Box>
+  </FormControl>
+);
+
+export const ExpenseForm = ({ onSubmit }: IExpenseForm) => {
+  const { register, handleSubmit, formState } = useForm<Omit<Expense, "id">>({
+    resolver: zodResolver(expenseSchema),
+  });
+
+  const { errors, isSubmitting } = formState;
+
+  return (
+    <chakra.form
+      w="100%"
+      onSubmit={handleSubmit((d) =>
+        onSubmit({ ...d, amount: Number(d.amount) })
+      )}
+      id="expense-form"
+    >
+      <Stack w="100%" spacing={5}>
+        <FormField label="Item" error={errors.item}>
+          <Input {...register("item")} type="text" />
+        </FormField>
+        <FormField label="Category" error={errors.category}>
+          <Select {...register("category")} w="full">
+            <option value="Food">Food</option>
+            <option value="Furniture">Furniture</option>
+            <option value="Accessory">Accessory</option>
+          </Select>
+        </FormField>
+        <FormField label="Amount" error={errors.amount}>
+          <Input {...register("amount")} type="text" />
+        </FormField>
+        <Flex justifyContent="end">
+          <Button
+            id="expense-form"
+            colorScheme="teal"
+            isLoading={isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Flex>
+      </Stack>
+    </chakra.form>
+  );
+};
