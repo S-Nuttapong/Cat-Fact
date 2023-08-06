31edd05f8f2aba9f7ef65304db4d1e1bb6895900
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-08-06 16:53:10 +0700

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defaultExclude, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        exclude: [...defaultExclude, 'e2e/**/*'],
+        include: ['./src/**/*.spec.*']
+    },
+})
