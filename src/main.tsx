1a759e2201c131ade795042e153888ffe187d85d
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-08-07 09:43:32 +0700

    fix: build failure

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import {
-  ApiProvider,
-  LocalStorageExpenseService,
-  fetchCatFact,
-} from "./shared/apis";
-
+import { ApiProvider } from "./shared/apis";
 import { theme } from "./theme";
-
-const expenseDetailServices = new LocalStorageExpenseService();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApiProvider api={{ ...expenseDetailServices, fetchCatFact }}>
+    <ApiProvider>
       <QueryClientProvider client={new QueryClient()}>
         <ThemeProvider theme={theme}>
           <App />
